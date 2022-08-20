c5ad2da491380dbb6a1beb89a227f206df2e268e
Robert Pyke <robert.j.pyke@gmail.com>
2022-08-19 23:04:20 -0700

    Adds basic window setup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
 mod ecs;
 
-use bevy::prelude::*;
+use bevy::{
+    log::{Level, LogSettings},
+    prelude::*,
+    time::FixedTimestep,
+    window::PresentMode,
+};
 use ecs::systems;
 
+const TIMESTEP_EXAMPLE: f64 = 1.0;
+
 fn main() {
-    App::new().add_system(systems::example::hello_world).run();
+    App::new()
+        .insert_resource(WindowDescriptor {
+            width: 900.,
+            height: 900.,
+            title: "BevyJam2".to_string(),
+            present_mode: PresentMode::Fifo,
+            ..default()
+        })
+        .insert_resource(LogSettings {
+            level: Level::DEBUG,
+            filter: "wgpu=error,bevy_render=info,bevy_ecs=info".to_string(),
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(systems::camera::setup_camera)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(TIMESTEP_EXAMPLE))
+                .with_system(systems::example::hello_world),
+        )
+        .run();
 }
